c1f0f8115e5900524f9940a1d8331ad872675357
David Baker <dpbaker1298@gmail.com>
2023-11-19 21:02:03 -0500

    Add routes for creating and getting customers

--- app/app.go
+++ app/app.go
@@ -11,9 +11,22 @@ func Start() {
 	// mux := http.NewServeMux()
 	router := mux.NewRouter()
 	// define a route
-	router.HandleFunc("/greet", greet)
-	router.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	// start the server
 	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	customerId := vars["customer_id"]
+	w.Write([]byte(customerId))
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Post request received"))
 }
