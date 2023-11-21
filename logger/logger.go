090a46ca8a155b218f2d1c62323172d86ab82cd3
David Baker <dpbaker1298@gmail.com>
2023-11-21 10:47:57 -0500

    Update logger configuration and use logger.Info() in main.go Configure specific keys and formats from the zap logger config docs.

--- logger/logger.go
+++ logger/logger.go
@@ -1,13 +1,32 @@
 package logger
 
-import "go.uber.org/zap"
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
-var Log *zap.Logger
+var log *zap.Logger
 
 func init() {
 	var err error
-	Log, err = zap.NewProduction()
+
+	config := zap.NewProductionConfig()
+
+	encoderConfig := zap.NewDevelopmentEncoderConfig()
+	encoderConfig.TimeKey = "timestamp"
+	encoderConfig.LevelKey = "level"
+	encoderConfig.CallerKey = "caller"
+	encoderConfig.MessageKey = "message"
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncoderConfig = encoderConfig
+
+	log, err = config.Build()
+	// log, err = zap.NewProduction(zap.AddCallerSkip(1))
 	if err != nil {
 		panic(err)
 	}
+}
+
+func Info(message string, fields ...zap.Field) {
+	log.Info(message, fields...)
 }
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"log"
-
 	"github.com/Dbaker1298/banking/app"
+	"github.com/Dbaker1298/banking/logger"
 )
 
 func main() {
-	log.Println("Starting the application...")
+	// log.Println("Starting the application...")
+	logger.Info("Starting the application...")
 	app.Start()
 }
