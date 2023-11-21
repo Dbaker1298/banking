a63ca8f3cd098bbeece7703899779c803a364e15
David Baker <dpbaker1298@gmail.com>
2023-11-21 13:13:26 -0500

    Refactor Customer status conversion logic

--- domain/customer.go
+++ domain/customer.go
@@ -14,19 +14,24 @@ type Customer struct {
 	Status      string
 }
 
-func (c Customer) ToDto() *dto.CustomerResponse {
+// Attaching to the Domain Object
+func (c Customer) StatusAsText() string {
 	statusAsText := "active"
 	if c.Status == "0" {
 		statusAsText = "inactive"
 	}
 
+	return statusAsText
+}
+
+func (c Customer) ToDto() *dto.CustomerResponse {
 	return &dto.CustomerResponse{
 		Id:          c.Id,
 		Name:        c.Name,
 		City:        c.City,
 		Zipcode:     c.Zipcode,
 		DateOfBirth: c.DateOfBirth,
-		Status:      statusAsText,
+		Status:      c.StatusAsText(),
 	}
 }
 
